375443b7e865c10eb85f4ec7684b39d6777eabba
emmanoelcardoos <Emmanoel.cardoso09@gmail.com>
2024-12-17 15:39:17 +0000

    Corrige estrutura de pastas e arquivos do pacote ciasAI

--- ciasAI/__init__.py
+++ ciasAI/__init__.py
@@ -1,2 +1,15 @@
-# __init__.py dentro de ciasAI
-from ciasAI.leitor_dados import open_file
+# __init__.py
+from .leitor_dados import open_file
+from .analise_sentimento import (
+    contador_sentiment,
+    positive_tweet,
+    porcentage_sentimento,
+    filtro_tweet,
+)
+from .analise_cia import (
+    realizar_login,
+    total_tweets_por_companhia,
+    listar_companhias,
+    filtrar_tweets_por_companhia,
+    companhia_com_mais_tweets_negativos,
+)
